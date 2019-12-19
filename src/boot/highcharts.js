151b90a311c0bbf5aca3788c10fc5ee9dd7b50af
Desmond <sseekiat@dso.org.sg>
2019-12-19 18:58:15 +0800

    Added maps

--- src/boot/highcharts.js
+++ src/boot/highcharts.js
@@ -1,10 +1,15 @@
 import Highcharts from 'highcharts'
 import HighchartsVue from 'highcharts-vue'
 import highchartsMore from 'highcharts/highcharts-more'
+import mapInit from 'highcharts/modules/map'
+import addWorldMap from '../js/worldmap.js'
 
 // "async" is optional
 export default async ({ Vue }) => {
   console.log('> boot highcharts')
+  mapInit(Highcharts)
+  addWorldMap(Highcharts)
   highchartsMore(Highcharts)
   Vue.use(HighchartsVue, { tagName: 'charts' })
+  console.log('< boot highcharts')
 }
--- src/pages/AnalyticsPages/Maps.vue
+++ src/pages/AnalyticsPages/Maps.vue
@@ -1,11 +1,58 @@
 <template>
   <q-page class="q-pa-lg">
     <h1>Maps</h1>
+      <charts :constructor-type="'mapChart'" :options="mapOptions" class="map"></charts>
+    <h1>Maps</h1>
   </q-page>
 </template>
 
 <script>
+import mapData from '../../js/mapData'
+
+export default {
+  data () {
+    return {
+      mapOptions: {
+        chart: {
+          map: 'myMapName'
+        },
+        title: {
+          text: 'Highmaps basic demo'
+        },
+        subtitle: {
+          text: 'Source map: <a href="http://code.highcharts.com/mapdata/custom/world.js">World, Miller projection, medium resolution</a>'
+        },
+        mapNavigation: {
+          enabled: true,
+          buttonOptions: {
+            alignTo: 'spacingBox'
+          }
+        },
+        colorAxis: {
+          min: 0
+        },
+        series: [{
+          name: 'Random data',
+          states: {
+            hover: {
+              color: '#BADA55'
+            }
+          },
+          dataLabels: {
+            enabled: true,
+            format: '{point.name}'
+          },
+          allAreas: true,
+          data: mapData.array
+        }]
+      }
+    }
+  }
+}
 </script>
 
 <style>
+.map {
+  min-height: 500px;
+}
 </style>
